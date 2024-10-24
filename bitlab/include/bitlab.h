00dbc588365c21e0132e2d83b604ea287780cb8b
milosz275 <miloszmaculewicz@gmail.com>
2024-10-25 00:00:23 +0200

    Update style

--- bitlab/include/bitlab.h
+++ bitlab/include/bitlab.h
@@ -9,11 +9,11 @@ typedef enum
 
 /**
  * Run the BitLab program.
- * 
+ *
  * @param argc The number of arguments.
  * @param argv The arguments.
  * @return The result of the BitLab program.
  */
 bitlab_result run_bitlab(int argc, char* argv[]);
 
-#endif
+#endif // __BITLAB_H
