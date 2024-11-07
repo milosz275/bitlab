425c6065403a8e3a9f2cfaea28a1a1baa6e900ff
milosz275 <miloszmaculewicz@gmail.com>
2024-11-07 22:42:21 +0100

    Add command line arguments execution

--- bitlab/src/bitlab.c
+++ bitlab/src/bitlab.c
@@ -12,9 +12,6 @@
 
 bitlab_result run_bitlab(int argc, char* argv[])
 {
-    // [ ] utilize the argc and argv parameters.
-    if (argc != 1 && argv != NULL) {}
-
     // init
     init_config_dir();
     init_logging(BITLAB_LOG);
@@ -25,6 +22,32 @@ bitlab_result run_bitlab(int argc, char* argv[])
     if (pthread_create(&cli_thread, NULL, handle_cli, (void*)NULL) != 0)
         log_message(LOG_WARN, BITLAB_LOG, __FILE__, "CLI thread creation failed: %s", strerror(errno));
     log_message(LOG_INFO, BITLAB_LOG, __FILE__, "CLI thread started");
+
+    // execute command line arguments
+    if (argc > 1 && argv != NULL)
+    {
+        int total_length = 0;
+        for (int i = 1; i < argc; ++i)
+            total_length += strlen(argv[i]) + 1;
+
+        char* line = (char*)malloc(total_length * sizeof(char));
+        if (line == NULL)
+            log_message(LOG_ERROR, BITLAB_LOG, __FILE__, "Failed to allocate memory for command line arguments");
+        else
+        {
+            line[0] = '\0';
+            for (int i = 1; i < argc; ++i)
+            {
+                strcat(line, argv[i]);
+                if (i < argc - 1)
+                {
+                    strcat(line, " ");
+                }
+            }
+            cli_exec_line(line);
+            free(line);
+        }
+    }
 
     // main loop
     while (!get_exit_flag(&state))
