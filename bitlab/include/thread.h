aea0c9f8afc181b9ad2fdd92a56dbabf728d731f
milosz275 <miloszmaculewicz@gmail.com>
2024-11-14 22:16:35 +0100

    Add docs, add thread name

--- bitlab/include/thread.h
+++ bitlab/include/thread.h
@@ -4,6 +4,13 @@
 #include <stdio.h>
 #include <pthread.h>
 
-pthread_t thread_runner(void* (*start_routine)(void*), void* arg);
+/**
+ * Thread runner function. This function is used to create a new thread.
+ *
+ * @param start_routine The function pointer to the start routine.
+ * @param name The name of the thread.
+ * @param arg The argument to pass to the start routine.
+ */
+pthread_t thread_runner(void* (*start_routine)(void*), const char* name, void* arg);
 
 #endif // __THREAD_H
