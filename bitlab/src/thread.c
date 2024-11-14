6eb1840f1b8dc523d805b012c40128299a1564ed
milosz275 <miloszmaculewicz@gmail.com>
2024-11-14 22:19:56 +0100

    Add thread name

--- bitlab/src/thread.c
+++ bitlab/src/thread.c
@@ -6,11 +6,11 @@
 
 #include "utils.h"
 
-pthread_t thread_runner(void* (*start_routine)(void*), void* arg)
+pthread_t thread_runner(void* (*start_routine)(void*), const char* name, void* arg)
 {
     pthread_t thread;
     if (pthread_create(&thread, NULL, start_routine, arg) != 0)
-        log_message(LOG_WARN, BITLAB_LOG, __FILE__, "%s thread creation failed: %s", __func__, strerror(errno));
-    log_message(LOG_INFO, BITLAB_LOG, __FILE__, "%s thread started", __func__);
+        log_message(LOG_WARN, BITLAB_LOG, __FILE__, "%s thread creation failed: %s", name, strerror(errno));
+    log_message(LOG_INFO, BITLAB_LOG, __FILE__, "%s thread started", name);
     return thread;
 }
