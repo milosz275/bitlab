0e44c119119adef49c15ec7fa14ebbd5894b77ad
milosz275 <miloszmaculewicz@gmail.com>
2024-11-14 22:15:42 +0100

    Add numeric and domain address checks

--- bitlab/include/ip.h
+++ bitlab/include/ip.h
@@ -4,33 +4,49 @@
 
 
 /**
- * Get the local IP address of the machine.
+ * Get the local IP address of the machine (e.g. 192.168.1.10).
  *
  * @param ip_address The buffer to store the IP address.
  */
 void get_local_ip_address(char* ip_address);
 
 /**
- * Get the remote IP address of the machine.
+ * Get the remote IP address of the machine (e.g. 1.1.1.1).
  *
  * @param ip_address The buffer to store the IP address.
  */
 void get_remote_ip_address(char* ip_address);
 
 /**
- * Get the IP address of the URL.
+ * Get the IP address of the domain address (e.g. google.com).
  *
- * @param url The URL to get the IP address.
+ * @param addr The domain address.
  * @param ip_address The buffer to store the IP address.
  */
-void get_ip_of_url(const char* url, char* ip_address);
+void get_ip_of_address(const char* url, char* ip_address);
 
 /**
- * Check if the IP address is in the private network.
+ * Check if the IP address is in the private network (e.g. 192.168.1.10).
  *
  * @param ip_address The IP address to check.
  * @return 1 if the IP address is in the private network, 0 otherwise.
  */
 int is_in_private_network(const char* ip_address);
+
+/**
+ * Check if the IP address is a numeric address (e.g. 1.1.1.1 is, example.com is not).
+ *
+ * @param ip_address The IP address to check.
+ * @return 1 if the IP address is a numeric address, 0 otherwise.
+ */
+int is_numeric_address(const char* ip_address);
+
+/**
+ * Check if the IP address is a valid domain address (e.g. example.com is, example or 1.1.1.1 is not).
+ *
+ * @param ip_address The IP address to check.
+ * @return 1 if the IP address is a valid domain address, 0 otherwise.
+ */
+int is_valid_domain_address(const char* ip_address);
 
 #endif // __IP_H
