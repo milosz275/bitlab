396a657eb95ed9ae8f80a9bc71e90cc228d25d1f
milosz275 <miloszmaculewicz@gmail.com>
2024-11-16 18:48:18 +0100

    Add simple seed lookup for peer discovery

--- bitlab/src/peer_discovery.c
+++ bitlab/src/peer_discovery.c
@@ -1,8 +1,11 @@
 #include "peer_discovery.h"
 
+#include <string.h>
+
 #include "state.h"
 #include "utils.h"
 #include "cli.h"
+#include "ip.h"
 
 void* handle_peer_discovery(void* arg)
 {
@@ -11,7 +14,30 @@ void* handle_peer_discovery(void* arg)
     {
         if (get_peer_discovery())
         {
-            // peer discovery logic
+            start_peer_discovery_progress();
+            char ip_address[BUFFER_SIZE];
+            lookup_address("seed.bitcoin.sipa.be.", ip_address);
+            char* token = strtok(ip_address, " ");
+            int peer_count = 0;
+            while (token != NULL && *token != '\0')
+            {
+                if (strcmp(token, "0.0.0.0") == 0)
+                {
+                    log_message(LOG_ERROR, BITLAB_LOG, __FILE__, "Peer discovery failed: resolved IP address is 0.0.0.0");
+                    finish_peer_discovery_progress(false);
+                    break;
+                }
+                guarded_print_line("Found peer: %s", token);
+                token = strtok(NULL, " ");
+                peer_count++;
+            }
+            if (peer_count > 0)
+                finish_peer_discovery_progress(true);
+            else
+            {
+                log_message(LOG_ERROR, BITLAB_LOG, __FILE__, "Peer discovery failed: no peers found");
+                finish_peer_discovery_progress(false);
+            }
         }
         usleep(100000); // 100 ms
     }
