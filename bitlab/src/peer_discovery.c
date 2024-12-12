3c8491a3c8452895d2d58e7174bfe2e3fa548402
milosz275 <miloszmaculewicz@gmail.com>
2024-12-12 18:33:09 +0100

    Add DNS lookup seeds, add hardcoded peers, add program state handling for peerdiscovery thread, add peers aggregation in peer queue

--- bitlab/src/peer_discovery.c
+++ bitlab/src/peer_discovery.c
@@ -1,40 +1,115 @@
 #include "peer_discovery.h"
 
 #include <string.h>
+#include <stdint.h>
+#include <netinet/in.h>
+#include <pthread.h>
 
+#include "peer_queue.h"
 #include "state.h"
 #include "utils.h"
 #include "cli.h"
 #include "ip.h"
+
+static const char* dns_seeds[] =
+{
+    "seed.bitcoin.sipa.be.",
+    "seed.btc.petertodd.org.",
+    "dnsseed.emzy.de.",
+    NULL
+};
+
+static const char* hardcoded_peers[] =
+{
+    "23.84.108.213:8333",
+    "87.207.45.218:8333",
+    "35.207.115.204:8333",
+    "65.108.202.25:8333",
+    NULL
+};
 
 void* handle_peer_discovery(void* arg)
 {
     usleep(1000000); // 1 s
     while (!get_exit_flag())
     {
-        if (get_peer_discovery())
+        if (get_peer_discovery() && !get_peer_discovery_in_progress() && !get_peer_discovery_succeeded())
         {
             start_peer_discovery_progress();
-            char ip_address[BUFFER_SIZE];
-            lookup_address("seed.bitcoin.sipa.be.", ip_address);
-            char* token = strtok(ip_address, " ");
             int peer_count = 0;
-            while (token != NULL && *token != '\0')
+            bool search = false;
+
+            if (get_peer_discovery_hardcoded_seeds())
             {
-                if (strcmp(token, "0.0.0.0") == 0)
+                // query hardcoded seeds
+                search = true;
+                for (int i = 0; hardcoded_peers[i] != NULL; ++i)
                 {
-                    log_message(LOG_ERROR, BITLAB_LOG, __FILE__, "Peer discovery failed: resolved IP address is 0.0.0.0");
-                    finish_peer_discovery_progress(false);
-                    break;
+                    add_peer_to_queue(hardcoded_peers[i], 0);
+                    guarded_print_line("Added hardcoded peer: %s", hardcoded_peers[i]);
+                    peer_count++;
                 }
-                guarded_print_line("Found peer: %s", token);
-                token = strtok(NULL, " ");
-                peer_count++;
             }
+            else if (get_peer_discovery_dns_lookup())
+            {
+                // query DNS seeds
+                search = true;
+
+                if (get_peer_discovery_dns_domain() == NULL)
+                {
+                    for (int i = 0; dns_seeds[i] != NULL; ++i)
+                    {
+                        char ip_address[BUFFER_SIZE];
+                        lookup_address(dns_seeds[i], ip_address);
+                        char* token = strtok(ip_address, " ");
+                        while (token != NULL && *token != '\0')
+                        {
+                            if (strcmp(token, "0.0.0.0") == 0)
+                            {
+                                log_message(LOG_ERROR, BITLAB_LOG, __FILE__, "Invalid IP from DNS seed: %s", dns_seeds[i]);
+                                break;
+                            }
+                            add_peer_to_queue(token, 8333);
+                            token = strtok(NULL, " ");
+                            peer_count++;
+                        }
+                    }
+                }
+                else
+                {
+                    char ip_address[BUFFER_SIZE];
+                    lookup_address(get_peer_discovery_dns_domain(), ip_address);
+                    char* token = strtok(ip_address, " ");
+                    while (token != NULL && *token != '\0')
+                    {
+                        if (strcmp(token, "0.0.0.0") == 0)
+                        {
+                            log_message(LOG_ERROR, BITLAB_LOG, __FILE__, "Invalid IP from DNS seed: %s", get_peer_discovery_dns_domain());
+                            break;
+                        }
+                        add_peer_to_queue(token, 8333);
+                        token = strtok(NULL, " ");
+                        peer_count++;
+                    }
+                }
+            }
+            else
+            {
+                log_message(LOG_ERROR, BITLAB_LOG, __FILE__, "Peer discovery operation or no valid state set");
+                finish_peer_discovery_progress(false);
+            }
+
+            // [ ] Implement peer processing using the Bitcoin P2P protocol
+
             if (peer_count > 0)
             {
                 log_message(LOG_INFO, BITLAB_LOG, __FILE__, "Peer discovery succeeded: found %d peers", peer_count);
                 finish_peer_discovery_progress(true);
+            }
+            else if (!search)
+            {
+                log_message(LOG_ERROR, BITLAB_LOG, __FILE__, "Peer discovery failed: no valid state set");
+                finish_peer_discovery_progress(false);
             }
             else
             {
