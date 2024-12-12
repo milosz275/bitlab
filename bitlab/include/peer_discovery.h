955354999dab0da30257a05d61d2c327158a30bb
milosz275 <miloszmaculewicz@gmail.com>
2024-12-12 18:31:07 +0100

    Update headers

--- bitlab/include/peer_discovery.h
+++ bitlab/include/peer_discovery.h
@@ -1,19 +1,6 @@
 #ifndef __PEER_DISCOVERY_H
 #define __PEER_DISCOVERY_H
 
-#include <stdint.h>
-#include <netinet/in.h>
-#include <pthread.h>
-
-#define MAX_PEERS 10
-
-typedef struct
-{
-    struct sockaddr_in address;
-    int port;
-    int is_active;
-} peer_info;
-
 /**
  * Initializes peer discovery based on configuration.
  */
@@ -25,15 +12,5 @@ void init_peer_discovery();
  * @param arg Not used. Write dummy code for no warning.
  */
 void* handle_peer_discovery(void* arg);
-
-/**
- * Send a "getaddr" message to a peer.
- */
-void send_getaddr_request(int socket_fd);
-
-/**
- * Process an "addr" message to update known peers.
- */
-void process_addr_message(int socket_fd);
 
 #endif
