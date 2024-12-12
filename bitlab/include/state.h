787f97bbf6a66b9e1a2853bbc39088b7addeb465
milosz275 <miloszmaculewicz@gmail.com>
2024-12-12 18:31:29 +0100

    Add definitions for peer discovery

--- bitlab/include/state.h
+++ bitlab/include/state.h
@@ -1,6 +1,12 @@
 #ifndef __STATE_H
 #define __STATE_H
 
+#define BITLAB_VERSION "0.1.0"
+
+#define PEER_DISCOVERY_DEFAULT_DAEMON false
+#define PEER_DISCOVERY_DEFAULT_HARDCODED_SEEDS false
+#define PEER_DISCOVERY_DEFAULT_DNS_LOOKUP true
+
 #include <signal.h>
 #include <stdbool.h>
 #include <unistd.h>
@@ -32,6 +38,10 @@ typedef struct
  * @param peer_discovery The peer discovery operation of the program.
  * @param peer_discovery_in_progress The peer discovery operation in progress.
  * @param peer_discovery_succeeded The peer discovery operation succeeded.
+ * @param peer_discovery_daemon The peer discovery operation as a daemon.
+ * @param peer_discovery_hardcoded_seeds The peer discovery operation with hardcoded seeds.
+ * @param peer_discovery_dns_lookup The peer discovery operation with DNS lookup.
+ * @param peer_discovery_dns_domain The peer discovery DNS domain field for custom DNS lookup.
  * @param operation_mutex The mutex to protect the operation.
  */
 typedef struct
@@ -39,6 +49,10 @@ typedef struct
     bool peer_discovery;
     bool peer_discovery_in_progress;
     bool peer_discovery_succeeded;
+    bool peer_discovery_daemon;
+    bool peer_discovery_hardcoded_seeds;
+    bool peer_discovery_dns_lookup;
+    char* peer_discovery_dns_domain;
     pthread_mutex_t operation_mutex;
 } program_operation;
 
@@ -56,6 +70,11 @@ extern program_operation operation;
  * Initialize the program state.
  */
 void init_program_state();
+
+/**
+ * Print the program state.
+ */
+void print_program_state();
 
 /**
  * Set the exit flag.
@@ -78,18 +97,15 @@ void mark_started_with_parameters();
 
 /**
  * Destroy the program state.
- *
- * @param state The program state to destroy.
  */
-void destroy_program_state(program_state* state);
+void destroy_program_state();
 
 /**
  * Initialize the program operation.
  *
- * @param operation The program operation to initialize.
  * @return 0 if successful, otherwise 1.
  */
-int init_program_operation(program_operation* operation);
+int init_program_operation();
 
 /**
  * Start the peer discovery progress. This function only starts the progress if the peer discovery operation is set.
@@ -107,9 +123,16 @@ void finish_peer_discovery_progress(bool succeeded);
  * Set the peer discovery operation. This function cannot stop the peer discovery operation if it is in progress. Use force_stop_peer_discovery instead.
  *
  * @param value The value to set.
- * @return 0 if successful, otherwise 1.
+ * @return True if successful, otherwise false.
  */
-int set_peer_discovery(bool value);
+bool set_peer_discovery(bool value);
+
+/**
+ * Force stop the peer discovery operation.
+ *
+ * @return True if successful, otherwise false.
+ */
+bool force_stop_peer_discovery();
 
 /**
  * Get the peer discovery operation.
@@ -126,10 +149,96 @@ bool get_peer_discovery();
 bool get_peer_discovery_in_progress();
 
 /**
- * Clear all program operations.
+ * Get the peer discovery succeeded state.
  *
- * @param operation The program operation to clear.
+ * @return The peer discovery succeeded state.
  */
-void destroy_program_operation(program_operation* operation);
+bool get_peer_discovery_succeeded();
+
+/**
+ * Set the peer discovery daemon state.
+ *
+ * @param value The value to set.
+ * @return True if successful, otherwise false.
+ */
+bool set_peer_discovery_daemon(bool value);
+
+/**
+ * Set the peer discovery hardcoded seeds state.
+ *
+ * @param value The value to set.
+ * @return True if successful, otherwise false.
+ */
+bool set_peer_discovery_hardcoded_seeds(bool value);
+
+/**
+ * Set the peer discovery DNS lookup state.
+ *
+ * @param value The value to set.
+ * @return True if successful, otherwise false.
+ */
+bool set_peer_discovery_dns_lookup(bool value);
+
+/**
+ * Get the peer discovery daemon state.
+ *
+ * @return The peer discovery daemon state.
+ */
+bool get_peer_discovery_daemon();
+
+/**
+ * Get the peer discovery hardcoded seeds state.
+ *
+ * @return The peer discovery hardcoded seeds state.
+ */
+bool get_peer_discovery_hardcoded_seeds();
+
+/**
+ * Get the peer discovery DNS lookup state.
+ *
+ * @return The peer discovery DNS lookup state.
+ */
+bool get_peer_discovery_dns_lookup();
+
+/**
+ * Set the peer discovery DNS domain.
+ *
+ * @param domain The domain to set.
+ * @return True if successful, otherwise false.
+ */
+bool set_peer_discovery_dns_domain(const char* domain);
+
+/**
+ * Get the peer discovery DNS domain.
+ *
+ * @return The peer discovery DNS domain.
+ */
+const char* get_peer_discovery_dns_domain();
+
+/**
+ * Clear all program operations.
+ */
+void destroy_program_operation();
+
+/**
+ * Get the program PID.
+ *
+ * @return The program PID.
+ */
+int get_pid();
+
+/**
+ * Get the program start time.
+ *
+ * @return The program start time.
+ */
+time_t get_start_time();
+
+/**
+ * Get the elapsed time since the program started.
+ *
+ * @return The elapsed time.
+ */
+int get_elapsed_time();
 
 #endif
