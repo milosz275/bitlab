00a7964727150d27e452e8bf3d81dfbf41cd32b9
milosz275 <miloszmaculewicz@gmail.com>
2024-12-12 18:33:54 +0100

    Add peer discovery traits to program operation, add state setters and getters

--- bitlab/src/state.c
+++ bitlab/src/state.c
@@ -16,7 +16,14 @@
  * @param exit_flag The exit flag of the program.
  * @param exit_flag_mutex The mutex to protect the exit flag.
  */
-program_state state = { 0, 0, 0, 0, PTHREAD_MUTEX_INITIALIZER };
+program_state state =
+{
+    0,
+    0, 
+    0,
+    0,
+    PTHREAD_MUTEX_INITIALIZER
+};
 
 /**
  * The program operation structure used to store the operation of the program. Only accessible in state.c directly, otherwise functions should be used.
@@ -24,9 +31,23 @@ program_state state = { 0, 0, 0, 0, PTHREAD_MUTEX_INITIALIZER };
  * @param peer_discovery The peer discovery operation of the program.
  * @param peer_discovery_in_progress The peer discovery operation in progress.
  * @param peer_discovery_succeeded The peer discovery operation succeeded.
+ * @param peer_discovery_daemon The peer discovery operation as a daemon.
+ * @param peer_discovery_hardcoded_seeds The peer discovery operation with hardcoded seeds.
+ * @param peer_discovery_dns_lookup The peer discovery operation with DNS lookup.
+ * @param peer_discovery_dns_domain The peer discovery DNS domain field for custom DNS lookup.
  * @param mutex The mutex to protect the operation.
  */
-program_operation operation = { false, false, false, PTHREAD_MUTEX_INITIALIZER };
+program_operation operation =
+{
+    false,
+    false,
+    false,
+    PEER_DISCOVERY_DEFAULT_DAEMON,
+    PEER_DISCOVERY_DEFAULT_HARDCODED_SEEDS,
+    PEER_DISCOVERY_DEFAULT_HARDCODED_SEEDS,
+    NULL,
+    PTHREAD_MUTEX_INITIALIZER
+};
 
 void init_program_state()
 {
@@ -37,6 +58,16 @@ void init_program_state()
 
     if (strcmp(getenv("USER"), "root") == 0)
         log_message(LOG_WARN, BITLAB_LOG, __FILE__, "Running as root is not recommended");
+}
+
+void print_program_state()
+{
+    guarded_print_line("Program uptime: %d", get_elapsed_time());
+    guarded_print_line("Program PID: %d", state.pid);
+    if (state.started_with_parameters)
+        guarded_print_line("Program started with CLI parameters");
+    else
+        guarded_print_line("Program started without CLI parameters");
 }
 
 void set_exit_flag(volatile sig_atomic_t flag)
@@ -61,15 +92,15 @@ void mark_started_with_parameters()
     pthread_mutex_unlock(&state.exit_flag_mutex);
 }
 
-void destroy_program_state(program_state* state)
+void destroy_program_state()
 {
-    pthread_mutex_destroy(&state->exit_flag_mutex);
+    pthread_mutex_destroy(&state.exit_flag_mutex);
 }
 
-int init_program_operation(program_operation* operation)
+int init_program_operation()
 {
-    pthread_mutex_init(&operation->operation_mutex, NULL);
-    operation->peer_discovery = false;
+    pthread_mutex_init(&operation.operation_mutex, NULL);
+    operation.peer_discovery = false;
     return 0;
 }
 
@@ -86,31 +117,34 @@ void start_peer_discovery_progress()
 void finish_peer_discovery_progress(bool succeeded)
 {
     pthread_mutex_lock(&operation.operation_mutex);
-    operation.peer_discovery = false;
     operation.peer_discovery_in_progress = false;
     operation.peer_discovery_succeeded = succeeded;
     pthread_mutex_unlock(&operation.operation_mutex);
 }
 
-int set_peer_discovery(bool value)
+bool set_peer_discovery(bool value)
 {
     pthread_mutex_lock(&operation.operation_mutex);
     if (operation.peer_discovery && operation.peer_discovery_in_progress && !value)
+    {
         log_message(LOG_WARN, BITLAB_LOG, __FILE__, "Peer discovery operation in progress, cannot stop. Did you mean force_stop_peer_discovery?");
+        pthread_mutex_unlock(&operation.operation_mutex);
+        return false;
+    }
     else
         operation.peer_discovery = value;
     pthread_mutex_unlock(&operation.operation_mutex);
-    return 0;
+    return true;
 }
 
-int force_stop_peer_discovery()
+bool force_stop_peer_discovery()
 {
     pthread_mutex_lock(&operation.operation_mutex);
     operation.peer_discovery = false;
     operation.peer_discovery_in_progress = false;
     log_message(LOG_WARN, BITLAB_LOG, __FILE__, "Peer discovery operation force-stopped");
     pthread_mutex_unlock(&operation.operation_mutex);
-    return 0;
+    return true;
 }
 
 bool get_peer_discovery()
@@ -129,9 +163,90 @@ bool get_peer_discovery_in_progress()
     return value;
 }
 
-void destroy_program_operation(program_operation* operation)
+bool get_peer_discovery_succeeded()
 {
-    pthread_mutex_destroy(&operation->operation_mutex);
+    pthread_mutex_lock(&operation.operation_mutex);
+    bool value = operation.peer_discovery_succeeded;
+    pthread_mutex_unlock(&operation.operation_mutex);
+    return value;
+}
+
+bool set_peer_discovery_daemon(bool value)
+{
+    pthread_mutex_lock(&operation.operation_mutex);
+    operation.peer_discovery_daemon = value;
+    pthread_mutex_unlock(&operation.operation_mutex);
+    return true;
+}
+
+bool set_peer_discovery_hardcoded_seeds(bool value)
+{
+    pthread_mutex_lock(&operation.operation_mutex);
+    operation.peer_discovery_hardcoded_seeds = value;
+    pthread_mutex_unlock(&operation.operation_mutex);
+    return true;
+}
+
+bool set_peer_discovery_dns_lookup(bool value)
+{
+    pthread_mutex_lock(&operation.operation_mutex);
+    operation.peer_discovery_dns_lookup = value;
+    pthread_mutex_unlock(&operation.operation_mutex);
+    return true;
+}
+
+bool get_peer_discovery_daemon()
+{
+    pthread_mutex_lock(&operation.operation_mutex);
+    bool value = operation.peer_discovery_daemon;
+    pthread_mutex_unlock(&operation.operation_mutex);
+    return value;
+}
+
+bool get_peer_discovery_hardcoded_seeds()
+{
+    pthread_mutex_lock(&operation.operation_mutex);
+    bool value = operation.peer_discovery_hardcoded_seeds;
+    pthread_mutex_unlock(&operation.operation_mutex);
+    return value;
+}
+
+bool get_peer_discovery_dns_lookup()
+{
+    pthread_mutex_lock(&operation.operation_mutex);
+    bool value = operation.peer_discovery_dns_lookup;
+    pthread_mutex_unlock(&operation.operation_mutex);
+    return value;
+}
+
+bool set_peer_discovery_dns_domain(const char* domain)
+{
+    pthread_mutex_lock(&operation.operation_mutex);
+    if (operation.peer_discovery_dns_domain != NULL)
+        free(operation.peer_discovery_dns_domain);
+    operation.peer_discovery_dns_domain = (char*)malloc(strlen(domain) + 1);
+    if (operation.peer_discovery_dns_domain == NULL)
+    {
+        log_message(LOG_ERROR, BITLAB_LOG, __FILE__, "Failed to allocate memory for peer discovery DNS domain");
+        pthread_mutex_unlock(&operation.operation_mutex);
+        return false;
+    }
+    strcpy(operation.peer_discovery_dns_domain, domain);
+    pthread_mutex_unlock(&operation.operation_mutex);
+    return true;
+}
+
+const char* get_peer_discovery_dns_domain()
+{
+    pthread_mutex_lock(&operation.operation_mutex);
+    const char* value = operation.peer_discovery_dns_domain;
+    pthread_mutex_unlock(&operation.operation_mutex);
+    return value;
+}
+
+void destroy_program_operation()
+{
+    pthread_mutex_destroy(&operation.operation_mutex);
 }
 
 int get_pid()
